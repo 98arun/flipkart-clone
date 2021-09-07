d6b3866525e40e47b009b77c1d6aa3cb342ebd6f
Arun <82587103+98arun@users.noreply.github.com>
2021-09-07 22:42:15 +0530

    fix modal

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,15 @@
 console.log("hey");
-let loginbtn = document.getElementById("sign-up");
-let closebtn = document.getElementById("closeBtn");
+let loginBtn = document.getElementById("sign-up");
+let closeBtn = document.getElementById("closeBtn");
 let popup = document.getElementById("pop-up");
 let newUser = document.getElementById("signup-link");
 let existingUser = document.getElementById("sign-in-btn");
 
-loginbtn.addEventListener("click", (e) => {
+loginBtn.addEventListener("click", (e) => {
   e.preventDefault(e);
   popup.style.display = "flex";
 });
-closebtn.addEventListener("click", (e) => {
+closeBtn.addEventListener("click", (e) => {
   e.preventDefault(e);
   popup.style.display = "none";
 });
@@ -17,15 +17,51 @@ let hide = document.getElementsByClassName("hide");
 let hidden = document.getElementsByClassName("hidden");
 newUser.addEventListener("click", (e) => {
   e.preventDefault(e);
-  console.log("clicked");
-  hidden.style.display = "none";
-  hide.style.display = "block";
+  // let hidde = document.getElementsByClassName("hidden");
+  // console.log("clicked", hidde);
+  hidden[0].style.display = "none";
+  hidden[1].style.display = "none";
+  hidden[2].style.display = "none";
+  hidden[3].style.display = "none";
+  hidden[4].style.display = "none";
+  hidden[5].style.display = "none";
+  hidden[6].style.display = "none";
+  hidden[7].style.display = "none";
+  hidden[8].style.display = "none";
+  hide[0].style.display = "block";
+  hide[1].style.display = "block";
+  hide[2].style.display = "block";
+  hide[3].style.display = "block";
+
+  // Array.from(hidde).forEach((element) => {
+  //   console.log("clicked", element);
+  //   hidde[0].style.display = "hidden";
+  // hide.style.display = "block";
+  // });
+  // for (let item of hidde) {
+  //   console.log(item);
+  // }
+  // hidden.style.display = "hidden";
+  // hide.style.display = "block";
 });
 existingUser.addEventListener("click", (e) => {
   e.preventDefault(e);
-  hidden.style.display = "block";
-  hide.style.display = "none";
+  hidden[0].style.display = "block";
+  hidden[1].style.display = "block";
+  hidden[2].style.display = "block";
+  hidden[3].style.display = "block";
+  hidden[4].style.display = "block";
+  hidden[5].style.display = "block";
+  hidden[6].style.display = "block";
+  hidden[7].style.display = "block";
+  hidden[8].style.display = "block";
+  hide[0].style.display = "none";
+  hide[1].style.display = "none";
+  hide[2].style.display = "none";
+  hide[3].style.display = "none";
+  // hidden.style.display = "block";
+  // hide.style.display = "hidden";
 });
 
-console.log(hide);
-console.log(hidden);
+// console.log(hide);
+// console.log(hidden);
